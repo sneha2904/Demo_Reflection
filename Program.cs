4acb0386f877fcc2b350aa548397e9e79d98e8f6
sneha.busireddy <sneha.busireddy@TRI03L-PF2RMHV2>
2021-10-20 14:10:13 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reflection;
+
+namespace Demo_Reflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Demo_Reflection");
+            Type T = typeof(int);
+            Console.WriteLine("Type of Name:{0}", T.Name);
+            Console.WriteLine("Full name of type defined:{0}", T.FullName);
+            Console.WriteLine("Type of Assembly which it is defined:{0}", T.Assembly);
+            Console.WriteLine("Information about Base type:{0}", T.BaseType);
+            DynamicReflection_Class1 obj1 = new DynamicReflection_Class1();
+            obj1.firstmethod();
+            obj1.secondmethod();
+
+        }
+    }
+}
---
+++ Reflection_Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Reflection
+{
+    class DynamicReflection_Class1
+    {
+        public DynamicReflection_Class1() { }
+        public DynamicReflection_Class1(String msg) { }
+        
+        public void firstmethod()
+        {
+            Console.WriteLine("Inside mthd 1..");
+        }
+        public void secondmethod()
+        {
+            Console.WriteLine("Inside mthd 2..");
+        }
+
+
+
+    }
+}
